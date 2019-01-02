7131a0d9b8637e7f9b8178c32a285896f47d1cdc
Damian <damianstasiek@gmail.com>
2019-01-02 17:13:35 +0100

    Update file

--- js/script.js
+++ js/script.js
@@ -1,4 +1,21 @@
 $('.menu__toggle').on('click', function () {
-    console.log('text');
     $('.menu__main').slideToggle('active');
 })
+
+const toogleMenu = document.querySelector(".menu-toogle");
+const menuNav = document.querySelector(".main-nav");
+const nav = document.querySelector('nav');
+
+
+/*....Add active class to the current link in position....*/
+$(document).ready(function () {
+    $('a[href*=\\#]').bind('click', function (e) {
+        e.preventDefault();
+        const target = $(this).attr("href");
+        console.log(target);
+        $('html, body').stop().animate({
+            scrollTop: $(target).offset().top
+        }, 600);
+        return false;
+    });
+});
